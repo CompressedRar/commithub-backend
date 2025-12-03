a570b25977e20ab4e0015dc5dbd908732f68d34c
CompressedRar <qwertythanzip1103@gmail.com>
2025-12-03 13:19:51 +0800

    papapapp

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from app import create_app, db
 
 application = create_app()
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     with application.app_context():
         db.create_all() 
-    application.run(debug=True, host='0.0.0.0', port=5000)"""
+    application.run(debug=True, host='0.0.0.0', port=5000) 
