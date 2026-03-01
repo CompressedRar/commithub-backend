fae4c7fef5d1595933cc245ad1883097c8b99c70
CompressedRar <qwertythanzip1103@gmail.com>
2026-03-01 14:38:58 +0800

    aws

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_app():
     CORS(app, supports_credentials=True)
     
     app.config["SECRET_KEY"] = "hannelore"
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("LOCAL_DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("AWS_DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
